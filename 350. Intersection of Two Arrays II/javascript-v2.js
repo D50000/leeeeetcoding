55f97335f2c335dc8283401798e253629035f42b
Eddie Hsu <aoe102198@gmail.com>
2022-06-26 02:38:59 +0800

    Update 350. Intersection of Two Arrays II/javascript-v2.js

---
+++ 350. Intersection of Two Arrays II/javascript-v2.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+  // BigO(2n)
+  const map = new Map();
+  const ans = new Array();
+  for (let i = 0; i < nums1.length; i++) {
+    if (!map.hasOwnProperty(nums1[i])) {
+      map[nums1[i]] = 1;
+    } else {
+      map[nums1[i]] += 1;
+    }
+  }
+  for (let i = 0; i < nums2.length; i++) {
+    if (map.hasOwnProperty(nums2[i])) {
+      if (map[nums2[i]] === 1) {
+        delete map[nums2[i]];
+      } else {
+        map[nums2[i]] -= 1;
+      }
+      ans.push([nums2[i]]);
+    }
+  }
+  return ans;
+};
