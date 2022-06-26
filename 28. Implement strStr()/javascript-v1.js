edee645942b72ffafeba71be8319c97bc248c2ef
Eddie Hsu <aoe102198@gmail.com>
2022-06-26 13:35:40 +0800

    Update 28. Implement strStr()/javascript-v1.js

---
+++ 28. Implement strStr()/javascript-v1.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  // Build in api.
+  return haystack.indexOf(needle);
+};
