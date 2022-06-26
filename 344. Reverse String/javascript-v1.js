8439e999245c554a6a85b2a75677ed7c9ccbadea
Eddie Hsu <aoe102198@gmail.com>
2022-06-26 12:08:36 +0800

    Update 344. Reverse String/javascript-v1.js

---
+++ 344. Reverse String/javascript-v1.js
@@ -0,0 +1,8 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  // Use the String.prototype.reverse.
+  s.reverse();
+};
